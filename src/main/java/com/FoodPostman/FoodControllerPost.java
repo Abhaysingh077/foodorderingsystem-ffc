bb3dd29fa6251eb69da1c6f5b7f1c76bd8517c08
Abhaysingh077 <abhay.singh@walkingtree.tech>
2022-06-24 17:39:07 +0530

    add

--- src/main/java/com/FoodPostman/FoodControllerPost.java
+++ src/main/java/com/FoodPostman/FoodControllerPost.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin(origins = "*")
 public class FoodControllerPost{
 	@Autowired
 	MenuService menuService;
